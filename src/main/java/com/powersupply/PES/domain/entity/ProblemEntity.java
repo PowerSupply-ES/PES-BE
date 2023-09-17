6bb62465c2db1faceb02d8478e70cd5f5a1ac320
JunhyeonKwak <jhkwak905@naver.com>
2023-09-17 16:43:48 +0900

    feat: add problemEntity

---
+++ src/main/java/com/powersupply/PES/domain/entity/ProblemEntity.java
@@ -0,0 +1,21 @@
+package com.powersupply.PES.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "problem_table")
+public class ProblemEntity extends BaseEntity{
+
+    private String problemTitle;
+    private String problemContent;
+    private int problemScore;
+}
