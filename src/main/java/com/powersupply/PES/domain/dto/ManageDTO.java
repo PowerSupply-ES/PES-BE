7ab122f62f07d5645c6347bb19fbeb680625d3a5
JunhyeonKwak <jhkwak905@naver.com>
2023-11-04 13:40:29 +0900

    feat: add manage member
    Signed-off-by: JunhyeonKwak <jhkwak905@naver.com>

--- src/main/java/com/powersupply/PES/domain/dto/ManageDTO.java
+++ src/main/java/com/powersupply/PES/domain/dto/ManageDTO.java
@@ -1,5 +1,6 @@
 package com.powersupply.PES.domain.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 public class ManageDTO {
@@ -8,5 +9,16 @@ public class ManageDTO {
     public static class makeQuestion {
         private String questionContent;
         private int questionDifficulty;
+    }
+
+    @Getter
+    @Builder
+    public static class MemberList {
+        private String memberStuNum; // 학번
+        private String memberName; // 이름
+        private int memberGen; // 기수
+        private String memberMajor; // 학과
+        private String memberStatus; // 상태
+        private String memberEmail; // 이메일
     }
 }
--- src/main/java/com/powersupply/PES/service/ManageService.java
+++ src/main/java/com/powersupply/PES/service/ManageService.java
@@ -1,15 +1,21 @@
 package com.powersupply.PES.service;
 
 import com.powersupply.PES.domain.dto.ManageDTO;
+import com.powersupply.PES.domain.dto.MemberDTO;
+import com.powersupply.PES.domain.entity.MemberEntity;
 import com.powersupply.PES.domain.entity.ProblemEntity;
 import com.powersupply.PES.domain.entity.QuestionEntity;
 import com.powersupply.PES.exception.AppException;
 import com.powersupply.PES.exception.ErrorCode;
+import com.powersupply.PES.repository.MemberRepository;
 import com.powersupply.PES.repository.ProblemRepository;
 import com.powersupply.PES.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +23,7 @@ public class ManageService {
 
     private final ProblemRepository problemRepository;
     private final QuestionRepository questionRepository;
+    private final MemberRepository memberRepository;
 
     // 질문 만들기
     @Transactional
@@ -31,5 +38,27 @@ public class ManageService {
                 .problemEntity(problemEntity)
                 .build();
         questionRepository.save(questionEntity);
+    }
+
+    // 멤버 관리하기
+    @Transactional
+    public List<ManageDTO.MemberList> getMemberList() {
+
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<ManageDTO.MemberList> memberListArrayList = new ArrayList<>();
+
+        for(MemberEntity memberEntity: memberEntityList) {
+            ManageDTO.MemberList memberList = ManageDTO.MemberList.builder()
+                    .memberStuNum(memberEntity.getMemberStuNum())
+                    .memberGen(memberEntity.getMemberGen())
+                    .memberName(memberEntity.getMemberName())
+                    .memberMajor(memberEntity.getDetailMemberEntity().getMemberMajor())
+                    .memberEmail(memberEntity.getDetailMemberEntity().getMemberEmail())
+                    .memberStatus(memberEntity.getMemberStatus())
+                    .build();
+            memberListArrayList.add(memberList);
+        }
+
+        return memberListArrayList;
     }
 }
