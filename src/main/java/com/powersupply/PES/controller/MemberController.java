7bd4891b4bcceedef3dbed5c2efb596d8158b1af
JunhyeonKwak <jhkwak905@naver.com>
2024-01-04 21:33:52 +0900

    feat: MemberController 수정
    Signed-off-by: JunhyeonKwak <jhkwak905@naver.com>

--- src/main/java/com/powersupply/PES/controller/MemberController.java
+++ src/main/java/com/powersupply/PES/controller/MemberController.java
@@ -9,51 +9,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
-
-    @GetMapping("")
-    public String home() {
-        return "redirect:/signin";
-    }
-
-    // 회원가입 페이지 불러오기
-    @GetMapping("/signup")
-    public String getSignUp() {
-        return "signup";
-    }
-
-    // 로그인 페이지 불러오기
-    @GetMapping("/signin")
-    public String getSignIn() {
-        return "signin";
-    }
-
-    // 계정 찾기 페이지 불러오기
-    @GetMapping("/finduser")
-    public String getFindUser() {
-        return "finduser";
-    }
-
-    // 마이페이지 불러오기
-    @GetMapping("/mypage")
-    public String getMyPage() {
-        return "mypage";
-    }
-
-    // 메인화면 불러오기
-    @GetMapping("/main")
-    public String getMain() {
-        return "main";
-    }
 
     // 회원가입 정보 저장
     @PostMapping("/api/signup")
@@ -90,22 +56,22 @@ public class MemberController {
     }
 
     // 비밀번호 찾기
-    @PostMapping("/api/finduser")
-    public ResponseEntity<?> findUser(@RequestBody MemberDTO.MemberFindPwRequest dto) {
-        memberService.findUser(dto);
-
-        return ResponseUtil.successResponse("가입한 이메일로 임시 비밀번호를 전송했습니다.");
-    }
+//    @PostMapping("/api/finduser")
+//    public ResponseEntity<?> findUser(@RequestBody MemberDTO.MemberFindPwRequest dto) {
+//        memberService.findUser(dto);
+//
+//        return ResponseUtil.successResponse("가입한 이메일로 임시 비밀번호를 전송했습니다.");
+//    }
 
     // 상단 사용자 정보
-    @GetMapping("/api/myuser")
-    public ResponseEntity<MemberDTO.NameScoreStatusResponse> myUser() {
-        return ResponseEntity.ok().body(memberService.myUser());
-    }
+//    @GetMapping("/api/myuser")
+//    public ResponseEntity<MemberDTO.NameScoreStatusResponse> myUser() {
+//        return ResponseEntity.ok().body(memberService.myUser());
+//    }
 
     // 랭킹 확인하기
-    @GetMapping("/api/rank")
-    public ResponseEntity<List<MemberDTO.NameScoreResponse>> memberRank() {
-        return ResponseEntity.ok().body(memberService.memberRank());
-    }
+//    @GetMapping("/api/rank")
+//    public ResponseEntity<List<MemberDTO.NameScoreResponse>> memberRank() {
+//        return ResponseEntity.ok().body(memberService.memberRank());
+//    }
 }
