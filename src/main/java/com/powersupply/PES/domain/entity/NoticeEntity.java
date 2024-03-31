913b4c13721cc02289e954c093623d59215669b3
JunhyeonKwak <jhkwak905@naver.com>
2024-03-31 22:13:46 +0900

    feat: 공지사항 관련 DB 추가
    Signed-off-by: JunhyeonKwak <jhkwak905@naver.com>

---
+++ src/main/java/com/powersupply/PES/domain/entity/NoticeEntity.java
@@ -0,0 +1,31 @@
+package com.powersupply.PES.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Table(name = "notice_table")
+public class NoticeEntity extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long noticeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writerId", referencedColumnName = "memberId")
+    private MemberEntity memberEntity;
+
+    @Column(nullable = false)
+    private String noticeTitle;
+    @Lob
+    @Column(nullable = false)
+    private String noticeContent;
+    private int noticeHit;
+    private boolean isImportant;
+}
