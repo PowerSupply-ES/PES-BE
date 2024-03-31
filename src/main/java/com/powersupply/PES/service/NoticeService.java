074d6b41ce0593694c112b829ef4f4c263866349
JunhyeonKwak <jhkwak905@naver.com>
2024-03-31 23:43:14 +0900

    chore: 공지사항관련 수정
    Signed-off-by: JunhyeonKwak <jhkwak905@naver.com>

--- src/main/java/com/powersupply/PES/service/NoticeService.java
+++ src/main/java/com/powersupply/PES/service/NoticeService.java
@@ -2,7 +2,9 @@ package com.powersupply.PES.service;
 
 import com.powersupply.PES.domain.dto.NoticeDTO;
 import com.powersupply.PES.domain.entity.NoticeEntity;
+import com.powersupply.PES.repository.MemberRepository;
 import com.powersupply.PES.repository.NoticeRepository;
+import com.powersupply.PES.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +15,17 @@ import org.springframework.stereotype.Service;
 public class NoticeService {
 
     private final NoticeRepository noticeRepository;
+    private final MemberRepository memberRepository;
 
     // 공지사항 등록
     public ResponseEntity<?> postNotice(NoticeDTO.createNotice dto) {
+        String memberId = JwtUtil.getMemberIdFromToken();
 
         NoticeEntity noticeEntity = NoticeEntity.builder()
                 .noticeTitle(dto.getTitle())
                 .noticeContent(dto.getContent())
                 .isImportant(dto.isImportant())
+                .memberEntity(memberRepository.findById(memberId).get())
                 .build();
         noticeRepository.save(noticeEntity);
 
